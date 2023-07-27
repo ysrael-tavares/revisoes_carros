4f57b3911ef6528e37f7b06fcc76357d016b2299
ysrael-tavares <51800219+ysrael-tavares@users.noreply.github.com>
2023-07-27 12:49:56 -0300

    Tabela Owners

---
+++ app/Models/Owner.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Owner extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'gender',
+        'date_of_birth',
+        'email',
+        'phone',
+    ];
+}
