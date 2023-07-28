a526e83b90d6590ac6e804936d297ad7806cd476
ysrael-tavares <51800219+ysrael-tavares@users.noreply.github.com>
2023-07-28 11:17:48 -0300

    Update Cars.vue

--- resources/js/Pages/Cars.vue
+++ resources/js/Pages/Cars.vue
@@ -82,7 +82,7 @@ export default {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Marcas</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Carros</h2>
         </template>
 
         <div class="py-12">
