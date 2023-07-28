590aa1dd7df0b30f225439a603f3236cb59564f8
ysrael-tavares <51800219+ysrael-tavares@users.noreply.github.com>
2023-07-28 09:45:00 -0300

    Componente de Tabela

---
+++ resources/js/Components/Table/TableData.vue
@@ -0,0 +1,11 @@
+<template>
+    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        <slot />
+    </td>
+</template>
+
+<script>
+export default {
+
+}
+</script>
