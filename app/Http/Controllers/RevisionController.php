32f2f8b0964bc2530802a6e695361748c52f32dd
ysrael-tavares <51800219+ysrael-tavares@users.noreply.github.com>
2023-07-28 11:49:08 -0300

    Tabela de revisões

---
+++ app/Http/Controllers/RevisionController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Revision;
+use Illuminate\Http\Request;
+
+class RevisionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'car_id' => ['required', 'numeric'],
+            'review_day' => ['required', 'date', 'before_or_equal:'.date('Y-m-d')]
+        ];
+
+        $data = $request->validate($rules);
+
+        $revision = Revision::create($data);
+
+        if($revision) return response()->json('Revisão inserida', 201);
+
+        return response()->json('Erro ao inserir revisão');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Revision $revision)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Revision $revision)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Revision $revision)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Revision $revision)
+    {
+        //
+    }
+}
