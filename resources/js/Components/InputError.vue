2fecafa604b5b56b55c1e6485fb498021b94fb65
ysrael-tavares <51800219+ysrael-tavares@users.noreply.github.com>
2023-07-29 10:39:44 -0300

    Ajuste para diferentes tipos de Message

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -8,7 +8,10 @@ defineProps({
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-red-600" v-for="text in message">
+        <p class="text-sm text-red-600" v-if="typeof message === 'string'">
+            {{message}}
+        </p>
+        <p class="text-sm text-red-600" v-for="text in message" v-else>
             {{ text }}
         </p>
     </div>
