3236a89c5adc82544d6b81404dbca3b44b0992c6
ysrael-tavares <51800219+ysrael-tavares@users.noreply.github.com>
2023-08-02 13:55:45 -0300

    Botão de Registro

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -70,6 +70,12 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <Link
+                    :href="route('register')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Não possui conta?
+                </Link>
                 <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Entrar
                 </PrimaryButton>
